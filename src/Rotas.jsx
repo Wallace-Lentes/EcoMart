2250f6f6f41f32e6d70072b472fe1859e38ec98a
isaabellepontes <isaapontes010@gmail.com>
2023-10-23 00:33:38 -0300

    Feat: Importando Rotas

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { GlobalStyle } from "./style/GlobalStyle"
 //import { tema } from "./src/style/theme"
-import LandingPage from "./pages/LandingPage/LandingPages"
 import QuemSomos from "./pages/QuemSomos/QuemSomos"
+import PaginaHome from './pages/PaginaHome/PaginaHome'
+import Login from "./pages/login/Login"
 
 const Rotas = () => {
 return (
     <BrowserRouter>
         <GlobalStyle />
         <Routes>
-        <Route path="/" component={<LandingPage />} />
+        <Route path="/" component={<PaginaHome />} />
         <Route path="/ Quem Somos" component={<QuemSomos />} />
+        <Route path="/ Login" component={<Login />} />
         </Routes>
     </BrowserRouter>
 )
