8bc99c5a63f9770db62b77300345ab90fc7e7c40
isaabellepontes <isaapontes010@gmail.com>
2023-10-22 22:22:49 -0300

    Fix: Mudando a pagina rotas de pasta

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { GlobalStyle } from "../EcoMart/src/style/GlobalStyle"
+import { GlobalStyle } from "./style/GlobalStyle"
 //import { tema } from "./src/style/theme"
-import LandingPage from "./src/pages/LandingPage/LandingPages"
-import QuemSomos from "./src/pages/QuemSomos/QuemSomos"
-import Login from './src/pages/login/Login'
+import LandingPage from "../src/pages/LandingPage/LandingPages"
+import QuemSomos from "../src/pages/QuemSomos/QuemSomos"
+import Login from '../src/pages/login/Login'
 
 const Rotas = () => {
 return (
