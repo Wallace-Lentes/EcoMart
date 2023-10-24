0f100a3be04198b29c9b0f31d6ba0d19c00365f8
isabellalcvicente <isabellavicente@ufpr.br>
2023-10-24 20:39:32 -0300

    feat: estilização da pagina produtos

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../common/Button/Button'
+import Notificacao from '../Notificacao/Notificacao'
+import styled from 'styled-components'
 
 const Card = ({id, titulo, img, valor, loja, descricao}) => {
+  
+  const [notificacaoAberta, setNotificacaoAberta] = useState(false);
+
+  const handleClick = () => {
+    setNotificacaoAberta(true);
+    setTimeout(() => {
+      setNotificacaoAberta(false);
+    }, 2000);
+  };
+
+
   return (
-    <div className="cardProduto">
-      <img src={img} alt={titulo} />
+    <StyledCard className="cardProduto">
+      <img className='imgProdutos' src={img} alt={titulo} />
       <h2>{titulo}</h2>
       <p>{descricao}</p>
       <p> {loja}</p>
       <p>Valor: ${valor}</p>
-      <button>Adicionar ao carrinho</button>
-    </div>
+      <Button texto='Adicionar ao carrinho' variant='primary' onClick={handleClick} />
+      
+    </StyledCard>
   )
 }
 
 export default Card
+
+const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+  margin-top: 24px;
+  margin-left: 20%;
+  gap: 16px;
+  max-width: 400px;
+  padding: 40px;
+  background: #95d195;
+  border-radius: 16px;
+  box-shadow:  9px 6px  #364936;
+`
