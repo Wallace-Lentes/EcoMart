19f351033221eb39d52173a0fb22f47e7c23d775
drmidn1ght <ana.drmidnight@gmail.com>
2023-10-25 20:46:59 -0300

    feat: Estilizando página Contato

--- src/pages/Contato/Contato.style.js
+++ src/pages/Contato/Contato.style.js
@@ -1,25 +1,49 @@
-import style from "styled-components";
+import styled from "styled-components"
+export const StyleContato = styled.main`
 
-export const styleContato = style.main`
-
-h2 {
-    color: 0A6B10;
-    font-size: 35px
+h2{
+    color: #0a6b10;
+    font-size: 35px;
     text-align: center;
-    padding: 2%
+    padding: 2%;
+}
+.Textoopiniao {
+    text-align: justify;
+    color: #0a6b10;
+    padding-left: 10%;
+    padding-right: 10%;
 }
 h3 {
+    font-size: 25px;
+    color: #0a6b10;
     text-align: center;
-    padding: 1%;
-    }
-input {
-    width: 30px;
-    height: 20px;
-    flex-direction: column;
+    padding: 2%;
 }
-.caixatexto {
-    width: 80px;
-    height: 60px;
-    
+.contatos,
+.phone1, .envelope1 {
+    display: flex;
+    padding-left: 11%;
+}
+.face,
+.insta {
+    padding-left: 11%;
+}
+p{
+    text-align: center;
+    padding-bottom: 3%;
+    padding-top: 3%;
+}
+.formulario{
+    padding-left:34%;
+}
+input {
+    display: block;
+    margin-bottom: 10px; 
+    width: 500px;
+    height:50px;
+}
+.caixatexto{
+    width: 500px;
+    height: 100px;
 }
 `
