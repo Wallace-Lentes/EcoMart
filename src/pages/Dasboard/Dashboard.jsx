7d867840d4c5b7fe1692050789989ea9d1e4eced
isabellalcvicente <isabellavicente@ufpr.br>
2023-10-25 21:51:42 -0300

    feat: tentativa de fazer o conversor

--- src/pages/Dasboard/Dashboard.jsx
+++ src/pages/Dasboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../../components/shared/Layout/Layout'
 import Footer from '../../components/views/LandingPage/Footer/Footer'
+import Conversor from '../../components/Conversor/Conversor'
 
 const Dashboard = () => {
   return (
     <div>
       <Layout/>
+      <Conversor/>
       <Footer/>
     </div>
     
