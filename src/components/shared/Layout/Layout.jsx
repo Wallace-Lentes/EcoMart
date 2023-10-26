579ea18837f01c9890aeb24b763c1e41a404325a
isaabellepontes <isaapontes010@gmail.com>
2023-10-25 23:14:58 -0300

    Feat: style da pagina inicial do projeto

--- src/components/shared/Layout/Layout.jsx
+++ src/components/shared/Layout/Layout.jsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom'
 // import { TransacoesContext } from '../../../core/contexto/useTransacoes'
 // import Logo from '../../common/Logo/Logo'
-import { StylesLayout } from './Layout.Styles'
+import {Recycle} from "@phosphor-icons/react";
+import { StylesLayout } from '../Layout/Layout.Styles'
 //   const { nome } = useContext(TransacoesContext)
 
 const Layout = () => {
@@ -17,17 +18,56 @@ const Layout = () => {
             <Link to='/Produtos'>Produtos</Link>
           </li>
           <li>
-            <Link>Contato</Link>
+            <Link to='/Contato'>Contato</Link>
           </li>
         </ul>
       </nav>
       <div>
-        <p>Olá, seja bem vindo</p>
-        <section className='corpo-pagina'>
-            <h1>Você possui 2392 pontos</h1>
-            <p>Troque seus pontos por benefícios</p>
+        <h1 className='titulo'>Olá, seja bem vindo</h1>
+            <h2>Atualmente você não possui pontos</h2>
+            <p>Faça sua primeira troca, acumule pontos e troque por beneficios</p>
+        <div className='coleta'>
+        <section className="sp">
+            <a
+            href="https://www.reciclasampa.com.br/pontos-de-coleta"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <Recycle color={'#013B0E'} size={32} />
+        </a>
+        <p> Ponto de coleta SP.</p>
         </section>
-
+        <section className="rj">
+            <a
+            href="https://www.recicloteca.org.br/coleta-seletiva/coleta-seletiva-carioca/"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <Recycle color={'#013B0E'} size={32} />
+        </a>
+        <p> Ponto de coleta RJ.</p>
+        </section>
+        <section className="sc">
+            <a
+            href="https://www.pmf.sc.gov.br/sistemas/comcap/ecopontos.php"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <Recycle color={'#013B0E'} size={32} />
+        </a>
+        <p> Ponto de coleta SC.</p>
+        </section>
+        <section className="rs">
+            <a
+            href="https://prefeitura.poa.br/carta-de-servicos/coleta-seletiva-de-residuos"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <Recycle color={'#013B0E'} size={32} />
+        </a>
+        <p> Ponto de coleta RS.</p>
+        </section>
+        </div>
       </div>
     </StylesLayout>
   )
