42d197d53f667d6fdd7eac40adc9baa4fcb4dd73
beafromsea <beatrizdoots@gmail.com>
2023-10-29 17:21:55 -0300

    fix: importação do layout e footer na página dashboard

--- src/pages/Dasboard/Dashboard.jsx
+++ src/pages/Dasboard/Dashboard.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import Conversor from '../../components/Conversor/Conversor'
-// import Layout from '../../components/shared/Layout/Layout'
-// import Footer from '../../components/views/LandingPage/Footer/Footer'
+import Layout from '../../components/shared/Layout/Layout'
+import Footer from '../../components/views/LandingPage/Footer/Footer'
 
 const Dashboard = () => {
   return (
     <div>
-      {/* <Layout/>
-      <Footer/>  */}
-      <Conversor />
+      <Layout/>
+      <Footer/>
     </div>
     
   )
